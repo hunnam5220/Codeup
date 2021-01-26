d33f122c4ef84e73662bff0c7c0dedfd9535d9f9
juuuun_b  <gudwns5220@gmail.com>
2021-01-26 19:00:48 +0900

    created_codeup_solution_2102

--- 03. DFS_BFS/solutions/2102. 배수 (Hard).py
+++ 03. DFS_BFS/solutions/2102. 배수 (Hard).py
@@ -1,4 +1,18 @@
-from sys import stdin
+n = int(input())
 
-n = int(stdin.readline().rstrip())
 
+def find_multiple_js(n):
+    b_num = 0b1
+    d_num = int(format(b_num, "b"), 10)
+
+    while 1:
+        if d_num % n == 0 :
+            return d_num
+        b_num += 1
+        d_num = int(format(b_num, "b"), 10)
+
+        if d_num >= 100000000000000000000:
+            return 0
+
+
+print(find_multiple_js(n))
